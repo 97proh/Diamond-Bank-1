1fc4d6351790610c3b3044402302f2963844f537
Guilherme <guilherme1706@hotmail.com>
2017-04-04 16:08:24 -0300

    Add files via upload
    PAGINA PRINCIPAL DO GERENTE

---
+++ gerente_index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+
+    <title>Diamond Bank</title>
+
+    
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+
+    
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    
+    <link href="css/style.css" rel="stylesheet">
+
+    
+</head>
+
+<body id="page-top" class="index">
+
+    <nav id="mainNav" class="navbar navbar-default navbar-custom navbar-fixed-top">
+        <div class="container">
+            
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">Diamond Bank</a>
+            </div>
+
+            
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#cadastrarCliente">Cadastrar Clientes</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Listar Clientes</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#">Sair</a>
+                    </li>
+                    
+                </ul>
+            </div>
+            
+        </div>
+        
+    </nav>
+
+   
+    <header>
+        <div class="container">
+            <div class="intro-text">
+                <div class="intro-lead-in">Você está logado como Gerente!</div>
+                <div class="intro-heading">Cadastre novos clientes agora mesmo.</div>
+                <a href="#cadastrarCliente" class="page-scroll btn btn-xl">Cadastrar clientes</a>
+            </div>
+        </div>
+    </header>
+   
+   
+
+    <!-- NESTA SEÇÃO O GERENTE VAI CADASTRAR OS CLIENTES-->
+    <section id="cadastrarCliente">
+        <div class="container">
+           
+        </div>
+    </section>
+
+    <!-- NESTA SEÇÃO VAI APARECE TODOS OS CLIENTES CADASTRADO NO SISTEMA-->
+    <section id="listarCliente">
+        <div class="container">
+           
+        </div>
+    </section>
+
+    
+
+    
+    
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <span class="copyright">Todos os direitos reservados &copy; Diamond Bank 2017</span>
+                </div>
+                <div class="col-md-4">
+                    <ul class="list-inline social-buttons">
+                        <li><a href="https://twitter.com/DiammondBank" target="_blank"><i class="fa fa-twitter"></i></a>
+                        </li>
+                        <li><a href="https://www.facebook.com/diammondbank/" target="_blank"><i class="fa fa-facebook"></i></a>
+                        </li>
+                        <li><a href="https://www.linkedin.com/in/diamond-bank-1a0a22140/" target="_blank"><i class="fa fa-linkedin"></i></a>
+                        </li>
+                    </ul>
+                </div>
+                
+            </div>
+        </div>
+    </footer>
+
+       
+
+    
+    <script src="jquery/jquery.js"></script>
+
+    
+    <script src="bootstrap/js/bootstrap.js"></script>
+
+   
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js" integrity="sha384-mE6eXfrb8jxl0rzJDBRanYqgBxtJ6Unn4/1F7q4xRRyIw7Vdg9jP4ycT7x1iVsgb" crossorigin="anonymous"></script>
+
+    
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    
+    <script src="js/agency.js"></script>
+
+</body>
+
+</html>
